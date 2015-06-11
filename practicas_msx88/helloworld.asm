19f1c36ce67b4dd9423deea179d20b1d908d3c2e
fmorisan <fmorisan@gmail.com>
2015-06-11 10:32:10 -0300

    Agregado inicio de prácticas en Assembler.

---
+++ practicas_msx88/helloworld.asm
@@ -0,0 +1,18 @@
+; hello world!
+; intento de hacer algo con MSX88
+
+; mem. datos
+org 1000h
+test DB 0ABCDh
+
+; mem. programa
+org 2000h
+mov BX, 0
+mov AL, 9
+suma:
+  add BX, AL
+  dec AL
+  jz suma
+
+; la suma de 9+8+7+6+5+4+3+2+1 está en BX
+
